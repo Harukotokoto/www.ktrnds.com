e2be643fd3483e9ab6ae9eaca8b5526779e76b5f
Rena <github@mail.ktrnds.com>
2024-02-03 06:51:39 +0900

    カバー画像を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
           property="og:description"
           content="Web開発がメインで、フロントエンド、バックエンドの作成が可能なフルスタックエンジニアです。主にReact(Next.js)を使用して開発しています。DiscordのBot作成からホームページ作成、API作成まで、幅広い開発を得意としています。Node.jsを使用していて、CSSが嫌いです。UIフレームワーク最高。ゲームもよくやってます。FPSゲーム、RPG、好きです。"
         />
-        <meta property="og:image" content="background.png" />
+        <meta property="og:image" content="https://www.ktrnds.com/background.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.ktrnds.com" />
         <meta
@@ -56,7 +56,7 @@ export default function RootLayout({
           property="twitter:description"
           content="Web開発がメインで、フロントエンド、バックエンドの作成が可能なフルスタックエンジニアです。主にReact(Next.js)を使用して開発しています。DiscordのBot作成からホームページ作成、API作成まで、幅広い開発を得意としています。Node.jsを使用していて、CSSが嫌いです。UIフレームワーク最高。ゲームもよくやってます。FPSゲーム、RPG、好きです。"
         />
-        <meta property="twitter:image" content="background.png" />
+        <meta property="twitter:image" content="https://www.ktrnds.com/background.png" />
       </head>
       <body>
         <ChakraProvider>{children}</ChakraProvider>

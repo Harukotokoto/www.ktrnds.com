1063ec2801d78f20dda61c780115f27593a1afe4
Rena <github@mail.ktrnds.com>
2024-02-03 06:48:39 +0900

    カバー画像を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <title>Harukoto - 先進的な技術的アイデアを。</title>
-        <meta name="title" content="Harukoto - 先進的な技術的アイデアを。" />
+        <title>Harukoto — 先進的な技術的アイデアを。</title>
+        <meta name="title" content="Harukoto — 先進的な技術的アイデアを。" />
         <meta
           name="description"
           content="Web開発がメインで、フロントエンド、バックエンドの作成が可能なフルスタックエンジニアです。主にReact(Next.js)を使用して開発しています。DiscordのBot作成からホームページ作成、API作成まで、幅広い開発を得意としています。Node.jsを使用していて、CSSが嫌いです。UIフレームワーク最高。ゲームもよくやってます。FPSゲーム、RPG、好きです。"
@@ -39,30 +39,24 @@ export default function RootLayout({
         <meta property="og:url" content="https://www.ktrnds.com" />
         <meta
           property="og:title"
-          content="Harukoto - 先進的な技術的アイデアを。"
+          content="Harukoto — 先進的な技術的アイデアを。"
         />
         <meta
           property="og:description"
           content="Web開発がメインで、フロントエンド、バックエンドの作成が可能なフルスタックエンジニアです。主にReact(Next.js)を使用して開発しています。DiscordのBot作成からホームページ作成、API作成まで、幅広い開発を得意としています。Node.jsを使用していて、CSSが嫌いです。UIフレームワーク最高。ゲームもよくやってます。FPSゲーム、RPG、好きです。"
         />
-        <meta
-          property="og:image"
-          content="background.svg"
-        />
+        <meta property="og:image" content="background.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.ktrnds.com" />
         <meta
           property="twitter:title"
-          content="Harukoto - 先進的な技術的アイデアを。"
+          content="Harukoto — 先進的な技術的アイデアを。"
         />
         <meta
           property="twitter:description"
           content="Web開発がメインで、フロントエンド、バックエンドの作成が可能なフルスタックエンジニアです。主にReact(Next.js)を使用して開発しています。DiscordのBot作成からホームページ作成、API作成まで、幅広い開発を得意としています。Node.jsを使用していて、CSSが嫌いです。UIフレームワーク最高。ゲームもよくやってます。FPSゲーム、RPG、好きです。"
         />
-        <meta
-          property="twitter:image"
-          content="background.svg"
-        />
+        <meta property="twitter:image" content="background.png" />
       </head>
       <body>
         <ChakraProvider>{children}</ChakraProvider>
